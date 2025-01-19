d77f44c4a1b05341bb8b0dad50f6b65569d67f88
abdaurhman-mabrouk <aabdo6629@gmail.com>
2025-01-19 10:02:26 +0200

    -

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Main from './Components/Sections/Main/Main';
 import Nav from './Components/Sections/Nav/Nav';
+import MessageDiv from './Components/Individuals/NavMessagesDiv/NavMessagesDiv';
 
 function App() {
   return (
     <>
       <Nav />
       {/* <Main /> */}
+      <MessageDiv />
     </>
   );
 }
---
+++ src/Components/Individuals/NavMessagesDiv/NavMessagesDiv.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NavMessagesDiv.css';
+
+function NavMessagesDiv() {
+  return <div></div>;
+}
+
+export default NavMessagesDiv;
